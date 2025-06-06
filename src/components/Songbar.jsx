a757849b5ed9b5bb915ad8227487ded4e3980c57
marcos marquez <asiaticokun@gmail.com>
2025-06-06 17:08:23 -0400

    feat: add nanostores for state management and update components

--- src/components/Songbar.jsx
+++ src/components/Songbar.jsx
@@ -1,11 +1,33 @@
-export default function () {
+import React from 'react';
+import { $cancionActual } from '../store';
+
+
+export default function() {
+
+    const [CancionActual, setCancionActual] = React.useState(null)
+    React.useEffect(() => {
+
+        $cancionActual.subscribe((estado) => {
+            setCancionActual(estado)
+            console.log("cancion actualizasa", estado)
+        })
+    },[])
+
+
+    
+
     return (
         <div className="songbar">
-            <h2 className="song-title">Song Title</h2>
-            <img src="https://via.placeholder.com/150" alt="Song Cover" className="song-image" />
+            <h2 className="song-title">{ CancionActual ? CancionActual.title : "no hay cancion seleccionada" }</h2>
+            <audio src={CancionActual ? CancionActual.audioURL: ""} autoPlay></audio>
+
+
+
+            {/* <img src="https://via.placeholder.com/150" alt="Song Cover" className="song-image" />
+            
             <h2 className="artist-name">Artist Name</h2>
             <p className="album-name">Album Name</p>
-            <button className="play-button">Play</button>
+            <button className="play-button">Play</button> */}
         </div>
-    );
+    )
 }
--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,3 +1,4 @@
+import { $cancionActual } from "../store"
 
 
 export default function Tarjeta({
@@ -6,23 +7,30 @@ export default function Tarjeta({
 }) {
 
     const handleClick = () => {
-        console.log("click en la cancion");
-        
+        $cancionActual.set({
+            title,
+            author, 
+            imageUrl,
+            audioURL,
+            album,
 
+        })
 
     }
 
 
-    return (
-        <div class="product-card">
-            <h2 class="product-title">{title}</h2>
-            <img src={imageUrl} alt="Product Title" class="product-image" />
-            <h2 class="artista">{author}</h2>
-            
-            <p class="album">Álbum: {album}</p>
-            <button onClick={handleClick}>play</button>
-        </div>
 
 
-    )
-}
+        return (
+            <div class="product-card">
+                <h2 class="product-title">{title}</h2>
+                <img src={imageUrl} alt="Product Title" class="product-image" />
+                <h2 class="artista">{author}</h2>
+
+                <p class="album">Álbum: {album}</p>
+                <button onClick={handleClick}>play</button>
+            </div>
+
+
+        )
+    }
